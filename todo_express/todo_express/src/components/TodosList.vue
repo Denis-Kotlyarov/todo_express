6fcb7471516101f7e8608558555b28dab2b6987d
Denis <156504836+Denis-Kotlyarov@users.noreply.github.com>
2024-05-22 18:52:49 +0300

    inialize

---
+++ todo_express/todo_express/src/components/TodosList.vue
@@ -0,0 +1,35 @@
+<template>
+    <main class="todos_list">
+        <h1>Ваши задачи</h1>
+        <ul>
+            <li v-for="todo in todos" :key="todo.id">
+                <p><strong>Задача: </strong> {{ todo.title }}</p>
+                <p><strong>Описание: </strong> {{ todo.body }}</p>
+                <p><strong>Состояние: </strong> {{ todo.done }}</p>
+            </li>
+        </ul>
+    </main>
+</template>
+
+<script setup>
+    import { onMounted } from 'vue';
+    import { ref } from 'vue';
+    
+    const todos = ref(null);
+
+    onMounted(() => {
+        fetch('http://localhost:3000/users')
+            .then((response) => response.json())
+                .then((result) => {
+                    todos.value = result
+                    console.log(todos.value)
+                })
+    })
+</script>
+
+<style>
+    .todos_list {
+        max-width: 1000px;
+        margin: 0 auto;
+    }
+</style>
---
+++ try_express/models/todo.js
@@ -0,0 +1,87 @@
+'use strict'
+
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+const path = require('path');
+
+class Todo {
+    constructor(title, body) {
+        this.id = uuidv4();
+        this.title = title;
+        this.body = body;
+        this.done = false;
+    }
+
+    static getAllTodos() {
+        return new Promise((res, rej) => {
+            fs.readFile(path.join(__dirname, '..', 'db', 'todos.json'), 'utf-8', (err, data) => {
+                if (err) {
+                    rej(err);
+                } else {
+                    res(JSON.parse(data));
+                }
+            })
+        })
+    }
+
+    async saveTodo() {
+        const todos = await Todo.getAllTodos();
+        todos.push({
+            id: uuidv4(),
+            title: this.title,
+            body: this.body,
+            done: false
+        })
+
+        return new Promise((res, rej) => {
+            fs.writeFile(path.join(__dirname, '..', 'db', 'todos.json'), JSON.stringify(todos), (err, data) => {
+                if (err) {
+                    rej(err);
+                } else {
+                    res();
+                }
+            })
+        })
+    }
+
+    static async updateTodo(id, data) {
+        const todos = await Todo.getAllTodos();
+        const index = todos.findIndex((t) => t.id === id);
+        const updatedCourse = {
+            id: id,
+            title: data.title,
+            body: data.body,
+            done: data.done,
+        }
+        todos[index] = updatedCourse;
+
+        new Promise((res, rej) => {
+            fs.writeFile(path.join(__dirname, '..', 'db', 'todos.json'), JSON.stringify(todos), (err, data) => {
+                if (err) {
+                    rej(err);
+                } else {
+                    res();
+                }
+            })
+        })
+
+        return updatedCourse;
+    }
+
+    static async deleteTodo(id) {
+        const todos = await Todo.getAllTodos();
+        const index = todos.findIndex((t) => t.id == id);
+        todos.splice(index, 1);
+        new Promise((res, rej) => {
+            fs.writeFile(path.join(__dirname, '..', 'db', 'todos.json'), JSON.stringify(todos), (err, data) => {
+                if (err) {
+                    rej(err);
+                } else {
+                    res(id);
+                }
+            })
+        })
+    }
+}
+
+module.exports = Todo
